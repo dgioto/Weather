80b9487e6c4d34cfb73be3d4cc956d4e652a6003
Alexander Tih <dgioto@gmail.com>
2022-07-12 14:58:01 +0300

    добавление первой карточки, с частичным заполнением

--- app/src/main/java/com/dgioto/weather/MainActivity.kt
+++ app/src/main/java/com/dgioto/weather/MainActivity.kt
@@ -3,12 +3,14 @@ package com.dgioto.weather
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import com.dgioto.weather.screens.MainScreen
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-
+            MainScreen()
         }
     }
 }
